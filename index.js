a1f9f6dcb7b5c22153ef219240d7477337dbcfca
River Ferguson <riverferguson@gmail.com>
2023-04-26 11:17:58 -0700

    add global variables

---
+++ index.js
@@ -0,0 +1,4 @@
+const dropdown = document.getElementsByClassName("#dropdown");
+const albumImage = document.querySelector("#album-image");
+const albumTitle = document.querySelector("#title");
+const releaseHeader = document.querySelector("#year-released");
